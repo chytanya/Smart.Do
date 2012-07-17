bea59594acce7a4afa39acafadc71ba1ce0372b5
Chaitanya Mutyala <chaitanya.mutyala@formativelearning.com>
2012-07-17 03:12:28 -0700

    Removed tags and tag triggers  from the code base

--- js/popup.js
+++ js/popup.js
@@ -8,9 +8,6 @@ Storage.prototype.getObject = function(key) {
 
 
 var max_todos = 5;
-var tag_list = [];
-
-
 
 var todos = { active : [] , completed: [] , active_count: 0, complete_count: 0 };
 //localStorage.removeItem('SmartDo.todos');
@@ -38,9 +35,7 @@ console.log('here');
 		events: {
 			'click	#app #create .content #create-btn' 			: 	'createNew',
 			'click  #app #create .content #cancel-btn'			: 	'cancelNew',
-			'keypress  #app #create #create-tag'				: 	'createTags',
 			'click  #app #todos .icon-trash'					: 	'deleteTask',
-			'click  #app #todos #todo-list .get-details'		: 	'search',
 			'mouseup  #app #todo-list .todo .text'				: 	'searchBySelection'
 		},
 
@@ -71,26 +66,10 @@ console.log('here');
 			$("#app #todo-details .content").html( todoDetailsTemplate({  }));
 		},
 
-		createTags: function(e){
-
-			var target = $(e.target);
-
-			if(e.keyCode == 13){
-				e.preventDefault();
-				var current_tag = $(target).text().trim();
-				tag_list.push(current_tag);
-				var current_tag_html = '<span class="label label-info">' + current_tag + '</span>';
-				$(target).parent().find('#tags-holder').append(current_tag_html);
-				$(target).text('');
-			}	
-		},
-
-
 		createNew: function(e){
 			var todos = localStorage.getObject('SmartDo.todos');
 			var newTodo = { 
 							label 			: 	$('#create #label').val(), 
-							tags 			:  	tag_list,
 							due_datetime 	: 	$("#create #schedule").datetimepicker( 'getDate' ), // moment($(".smart-do").datetimepicker( 'getDate' )).fromNow()
 							created 		: 	moment().toString(),
 							id 				:   'sd_' + moment().unix()
@@ -98,38 +77,11 @@ console.log('here');
 			todos.active.push(newTodo);
 			
 			localStorage.setObject('SmartDo.todos', todos);
-			tag_list = [];
 			this.render();
 		},
 
 		cancelNew: function(){
 			this.render();
-		},
-
-		search: function(e){
-
-			var todo_id = $(e.target).parents('.todo').attr('data-todo-id');
-			
-			// check if the todo has a valid ID
-			if(!todo_id){ console.log('No ID found for this DOM!'); return false; }
-			
-			// get all todos
-			var todos = localStorage.getObject('SmartDo.todos');
-			// Find the associated todo
-			var current_todo = _.find(todos.active, function(todo){
-				return todo.id == todo_id;
-			})
-			
-			// No matching record found
-			if(!current_todo){ console.log('No matching record found for ID: ', todo_id ); return false; }
-
-			var tags = current_todo.tags;
-			console.log('the tags', tags);
-
-			_.each(tags, function(tag){
-				initializeSearch(tag);
-			});
-			
 		},
 
 		deleteTask: function(e){
@@ -150,7 +102,8 @@ console.log('here');
 		searchBySelection: function(e){
 			var selectedText = getSelectionText()
 		    if (selectedText) {
-		      console.log('the selected text', selectedText); initializeSearch(selectedText.trim());
+		      //console.log('the selected text', selectedText); 
+		      initializeSearch(selectedText.trim());
 		    }
 		}
 
